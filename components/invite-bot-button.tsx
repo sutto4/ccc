99033d8c16cfb363aa6d642c2878c4f74a8b4607
sutto <133205772+sutto4@users.noreply.github.com>
2025-08-12 21:43:40 +1000

    Update invite-bot-button.tsx

--- components/invite-bot-button.tsx
+++ components/invite-bot-button.tsx
@@ -1,28 +1,26 @@
-"use client"
+"use client";
 
-import { Button, type ButtonProps } from "@/components/ui/button"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-export default function InviteBotButton(props: ButtonProps) {
-  const clientId = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID
-  const redirectUri = process.env.NEXT_PUBLIC_APP_BASE_URL
-    ? `${process.env.NEXT_PUBLIC_APP_BASE_URL}/auth/callback/discord`
-    : undefined
-  const scopes = ["bot", "applications.commands"]
-  const permissions = "268954736" // example permissions set
-  const inviteUrl = clientId
-    ? `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(
-        clientId,
-      )}&permissions=${permissions}&scope=${encodeURIComponent(scopes.join(" "))}${
-        redirectUri ? `&redirect_uri=${encodeURIComponent(redirectUri)}` : ""
-      }`
-    : "#"
+type Props = React.ComponentProps<typeof Button>;
+
+export default function InviteBotButton(props: Props) {
+  const clientId =
+    process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID ||
+    process.env.NEXT_PUBLIC_CLIENT_ID;
+
+  const href = clientId
+    ? `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=268437568&scope=bot%20applications.commands`
+    : "#";
+
+  const disabled = !clientId || props.disabled;
 
   return (
-    <Button asChild disabled={!clientId} {...props}>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href={inviteUrl} target={clientId ? "_blank" : undefined} rel={clientId ? "noreferrer" : undefined}>
-        Invite bot
-      </a>
+    <Button asChild disabled={disabled} {...props}>
+      <Link href={href} target="_blank" rel="noopener noreferrer">
+        Invite Bot
+      </Link>
     </Button>
-  )
+  );
 }
