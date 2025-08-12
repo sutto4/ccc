561e32e34496e73bda798d38e80cbfd938baa094
sutto <133205772+sutto4@users.noreply.github.com>
2025-08-12 22:51:22 +1000

    sd

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 const handler = NextAuth({
+  basePath: "/auth", // important: make callbacks use /auth, not /api/auth
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
